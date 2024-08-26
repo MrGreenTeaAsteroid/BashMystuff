3a8485a010082bc842179a789bb3d792d28347e7
MrGreenTea <113569183+MrGreenTeaAsteroid@users.noreply.github.com>
2024-08-25 21:52:30 -0400

    Add files via upload

---
+++ network.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+function install_apt() {
+    sudo apt-get install -y "$1"
+}
+
+install_apt nmap
+install_apt metasploit-framework
+install_apt wireshark
+install_apt tcpdump
+install_apt netcat
+install_apt hping3
+install_apt hydra
+install_apt john
+install_apt snort
+install_apt nikto
+install_apt nessus
+install_apt sqlmap
+install_apt git
+install_apt vim
+install_apt g++
+install_apt gcc
+install_apt cmake
+install_apt make
+install_apt valgrind
+install_apt gdb
+install_apt ctags
+install_apt clang
+install_apt clang-format
+install_apt python3-pip
+install_apt python3-dev
+install_apt python3-venv
+install_apt python3-wheel
+install_apt python3-setuptools
+install_apt python3-pytest
+install_apt python3-flake8
+install_apt python3-mypy
+install_apt docker
+install_apt docker-compose
+install_apt ansible
+install_apt kubernetes
+install_apt kubectl
+install_apt terraform
+install_apt vagrant
+install_apt jenkins
+install_apt puppet
+install_apt chef
+install_apt pycharm
+install_apt vscode
+install_apt intellij-idea
+install_apt postgresql
+install_apt mysql-server
+install_apt ruby
+install_apt ruby-dev
+install_apt bundler
+install_apt python3-flask
+install_apt python3-flask-sqlalchemy
+install_apt python3-flask-migrate
+install_apt python3-flask-wtf
+install_apt python3-flask-bootstrap
+install_apt python3-flask-mail
+install_apt gunicorn
+install_apt uwsgi
+install_apt nginx
+install_apt apache2
+install_apt ansible
+install_apt kubernetes
+install_apt kubectl
+install_apt terraform
+install_apt vagrant
+install_apt jenkins
+install_apt puppet
+install_apt chef
